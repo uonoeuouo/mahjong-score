e00e0be5de95b38df00be9cf3ed42460974c902f
ZEROYONN <reishi.k0702@gmail.com>
2025-07-21 14:44:29 +0900

    loggerの設定を修正
    loggerをmain関数に追加

--- logging_config.py
+++ logging_config.py
@@ -1,15 +1,25 @@
-from loguru import logger
 import sys
+from loguru import logger
+import os
 
 
 def setup_logging():
+    """Logging configuration.
+    This function should be called at the start of the application.
+    """
+
+    logs_dir = os.path.join(os.path.dirname(__file__), "logs")
     logger.remove()  # Remove the default logger
     logger.add(sys.stdout, level="INFO")
     logger.add(
-        "app.log", level="DEBUG", rotation="3MB", retention="30 days", compression="zip"
+        os.path.join(logs_dir, "app.log"),
+        level="DEBUG",
+        rotation="3MB",
+        retention="30 days",
+        compression="zip",
     )
     logger.add(
-        "error.log",
+        os.path.join(logs_dir, "error.log"),
         level="ERROR",
         rotation="5MB",
         retention="30 days",
